e3add8ec513d72365b3ba55fefd7a59ad295f0b3
BlueMarbleCode <116312747+BlueMarbleCode@users.noreply.github.com>
2024-09-19 18:01:03 -0400

    Add files via upload

---
+++ Learning Checkpoints/Checkpoint 1- Basic Concepts.py
@@ -0,0 +1,44 @@
+# ===================================================================
+# The Blue Marble Academy
+# Beginner Python Program
+# Learning Checkpoint # 1
+# Lesson 1 & 2: Basic Concepts Check 
+
+# Name: 
+# Date: 
+
+# ------------------------------------------------------------------------------
+# note: code is read from top to bottom!
+# ------------------------------------------------------------------------------
+# ===================================================================
+# 1. Declare a variable called name and assign your name to it
+
+# 2. Print the value of name using the print()
+
+# 3. Find the type() of the variable name
+
+# ===================================================================
+# 1. Declare variables called num1 and num2 with values 10 and 5.5 
+
+# 2. Add num1 and num2, store the result in a new variable
+
+# 3. Divide num1 by num2
+
+# 4. Multiply num1 by 10
+
+# 4. Print the results of the addition, division, and multipication
+
+# ===================================================================
+# 1. Declare variables first_name and last_name with your first and last name 
+
+# 2. Add first_name and last_name together to print your full name
+
+# 3. Print the length of last_name 
+
+# ===================================================================
+# 1. Declare a variable is_python_fun and assign it a boolean value
+
+# ===================================================================
+# Challenge: 
+# Write a program that asks the user for their age, calculates the year they were born, and prints a message saying "You were born in [year]."
+
